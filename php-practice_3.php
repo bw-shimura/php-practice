5259b4fdb490b955d7d7a7fc394fc81828133969
Haruka Shimura <y29814021@gmail.com>
2025-10-06 00:23:18 +0900

    問題2処理コード入力

--- php-practice_3.php
+++ php-practice_3.php
@@ -29,4 +29,34 @@ $pikatyuu = new Pokemon('ピカチュウ','ネズミ');
 
 $pikatyuu -> attack('10万ボルト');
 
+
+//追加課題2
+//社員の情報と出勤記録を管理するためのEmployeeクラスを作成
+
+//クラス名：Employee
+//プロパティ（１，employeeId 社員のID/正の整数　２，employeeName 社員名/任意の文字列）
+//コンストラクタ：社員IDと社員名をプロパティから呼び出せるように実装
+//メソッド：checkIn()メソッド　
+
+class Employee{
+
+  public $employeeId;
+  public $employeeName;
+
+  public function __construct($empId,$empName){
+    $this->employeeId = $empId;
+    $this->employeeName = $empName;
+  }
+
+  public function checkIn(){
+    echo $this->employeeName . 'が出勤しました。社員ID:' . $this->employeeId;
+  }
+}
+
+$staff = new Employee(1,'山田太郎');
+
+$staff -> checkIn();
+
+var_dump($staff);
+
 ?>
